c47db70d5da963423150b5a71d178f7f73662cdf
Fernando <fernando_kss@hotmail.com>
2021-05-18 23:07:11 -0300

    ajustes

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   subnet_ids = { for k, v in aws_subnet.this : v.tags.Name => v.id }
-
+  
   common_tags = {
     Project   = "AWS com Terraform"
     CreatedAt = "2021-05-13"
--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 terraform {
-  required_version = "0.14.8"
+  required_version = "0.15.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.32.0"
+      version = "3.40.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
+
   }
 
 }
